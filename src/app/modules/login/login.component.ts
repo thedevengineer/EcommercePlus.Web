5c769079ae67a9f18e8c595038c0357b6d1bbd0a
markrmrez <mrk.anthony.rmrez@gmail.com>
2020-09-19 17:21:47 +0800

    #1 Login Page UI completion and css cleanup

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit {
   loading = false;
   submitted = false;
   returnUrl: string;
-constructor(private formBuilder: FormBuilder){
+constructor(private formBuilder: FormBuilder, private router: Router){
 
 }
 
@@ -27,6 +28,7 @@ get f() { return this.loginForm.controls; }
 onSubmit() {
   console.log(this.f.username.value);
   console.log(this.f.password.value);
+  this.router.navigate(["/"]);
 }
 
 }
